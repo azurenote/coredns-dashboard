2da047de04b0928f439985aa63a3b03a46be3bbf
enseed <me@enseed.dev>
2022-08-13 14:27:15 +0900

    add materials module to group the material dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,27 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatListModule } from '@angular/material/list';
 import { ConfirmDialogComponent } from './components/confirm-dialog/confirm-dialog.component';
-import { MatDialogModule } from '@angular/material/dialog';
-import { GraphQLModule } from './graphql.module';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { MatCardModule } from '@angular/material/card';
 import { OverlayModule } from '@angular/cdk/overlay';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { MatTooltipModule } from '@angular/material/tooltip';
 import { UserInfoSummaryComponent } from './components/user-info-summary/user-info-summary.component';
 import { ZoneListComponent } from './components/zone-list/zone-list.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatTableModule } from '@angular/material/table';
+import { GraphQLModule } from './graphql/graphql.module';
 import { ZoneListViewComponent } from './views/zone-list-view/zone-list-view.component';
 import { ZoneRecordListComponent } from './components/zone-record-list/zone-record-list.component';
 import { ZoneDetailViewComponent } from './views/zone-detail-view/zone-detail-view.component';
+import { MaterialsModule } from './materials/materials.module';
 
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(http: HttpClient) {
@@ -45,15 +37,8 @@ export function HttpLoaderFactory(http: HttpClient) {
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSidenavModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule,
-    MatListModule,
-    MatDialogModule,
     GraphQLModule,
     HttpClientModule,
-    MatCardModule,
     OverlayModule,
     FlexLayoutModule,
     TranslateModule.forRoot({
@@ -64,8 +49,8 @@ export function HttpLoaderFactory(http: HttpClient) {
       },
       defaultLanguage: 'en-US'
     }),
-    MatTooltipModule,
-    MatTableModule
+    MaterialsModule,
+    GraphQLModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/materials/materials.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
+const MATERIALS = [
+  MatButtonModule,
+  MatCardModule,
+  MatDialogModule,
+  MatIconModule,
+  MatListModule,
+  MatSidenavModule,
+  MatTableModule,
+  MatToolbarModule,
+  MatTooltipModule,
+];
+
+@NgModule({
+  imports: MATERIALS,
+  exports: MATERIALS
+})
+export class MaterialsModule { }
