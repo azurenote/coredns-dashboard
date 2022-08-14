cb80364fda2a64afb6062e9035d2bab628b8543d
enseed <me@enseed.dev>
2022-08-15 00:11:20 +0900

    Update view structure

--- src/app/graphql/graphql.module.ts
+++ src/app/graphql/graphql.module.ts
@@ -3,7 +3,7 @@ import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
 import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
 import { HttpLink } from 'apollo-angular/http';
 
-const uri = '/graphql'; // <-- add the URL of the GraphQL server here
+const uri = 'http://m.loca:8000/graphql'; // <-- add the URL of the GraphQL server here
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
   return {
     link: httpLink.create({uri}),
