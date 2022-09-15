649b09dfe81c407c95584e3ce2909d5d2a68d490
enseed <me@enseed.dev>
2022-09-16 02:04:03 +0900

    try

--- src/app/components/zone-record-list/zone-record-list.component.spec.ts
+++ src/app/components/zone-record-list/zone-record-list.component.spec.ts
@@ -1,17 +1,24 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, flush, TestBed, waitForAsync } from '@angular/core/testing';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MatTableModule } from '@angular/material/table';
-import { ApolloTestingModule } from 'apollo-angular/testing';
+import { ApolloTestingController, ApolloTestingModule } from 'apollo-angular/testing';
 
 import { ZoneRecordListComponent } from './zone-record-list.component';
 import { RecordFormSelectorComponent } from '../record-form-selector/record-form-selector.component';
+import { By } from "@angular/platform-browser";
+import { Apollo } from "apollo-angular";
 
 describe('ZoneRecordListComponent', () => {
   let component: ZoneRecordListComponent;
   let fixture: ComponentFixture<ZoneRecordListComponent>;
+  let apollo: Apollo;
+
+  let controller: ApolloTestingController;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
       imports: [
         NoopAnimationsModule,
         ApolloTestingModule,
@@ -24,6 +31,10 @@ describe('ZoneRecordListComponent', () => {
     })
     .compileComponents();
 
+    apollo = TestBed.inject(Apollo);
+
+    controller = TestBed.inject(ApolloTestingController);
+
     fixture = TestBed.createComponent(ZoneRecordListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -33,12 +44,38 @@ describe('ZoneRecordListComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('has a data table', () => {
-    const compiled = fixture.debugElement.nativeElement;
-    const inputElement = compiled.querySelector('table#zone-record-list');
+  it('has a data table', fakeAsync(async () => {
+
+    component.ngOnInit();
+
+    controller.expectOne(component.QUERY).flush({
+      data:{
+        records: [
+          {
+            id: 0,
+            zone: 'test.',
+            name: 'test',
+            recordType: 'A',
+            ttl: 300,
+            createdAt: new Date(),
+            content: {
+              ip: '1.2.3.4'
+            }
+          }
+        ]
+      }
+    });
+
+    await fixture.whenStable();
+    fixture.detectChanges();
+
+    flush();
+
+    const { debugElement } = fixture;
+    const inputElement = debugElement.query(By.css('table#zone-record-list'));
 
     expect(inputElement)
       .withContext('Check if is there a table with the specified ID')
       .toBeTruthy();
-  });
+  }));
 });
--- src/app/graphql/graphql.module.ts
+++ src/app/graphql/graphql.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
 import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
 import { HttpLink } from 'apollo-angular/http';
+import { environment } from '../../environments/environment';
 
-const uri = 'http://localhost:8000/graphql';
+const uri = environment.apiHost + '/graphql';
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
   return {
     link: httpLink.create({uri}),
